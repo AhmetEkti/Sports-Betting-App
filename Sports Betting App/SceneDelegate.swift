e1c94ec6f86bf67bd1480f83712f3e54c52fc671
Ahmet Ekti <ahmetekti@gmail.com>
2024-10-14 11:41:44 +0300

    Update SceneDelegate.swift

--- Sports Betting App/SceneDelegate.swift
+++ Sports Betting App/SceneDelegate.swift
@@ -16,7 +16,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let _ = (scene as? UIWindowScene) else { return }
-        initializeApp()
     }
     
     func sceneDidDisconnect(_ scene: UIScene) {
@@ -45,11 +44,5 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Called as the scene transitions from the foreground to the background.
         // Use this method to save data, release shared resources, and store enough scene-specific state information
         // to restore the scene back to its current state.
-    }
-}
-
-extension SceneDelegate{
-    func initializeApp() {
-        AppInitializer.shared.initialize {}
     }
 }
